aaf0e1750532f11776e9418f1b8f0e815fe346f5
Trần Trọng Phúc <tranphucbol@gmail.com>
2019-04-07 22:00:00 +0700

    first

---
+++ models/users.js
@@ -0,0 +1,11 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const Users = sequelize.define('Users', {
+    username: DataTypes.STRING,
+    imagepath: DataTypes.STRING
+  }, {});
+  Users.associate = function(models) {
+    // associations can be defined here
+  };
+  return Users;
+};
